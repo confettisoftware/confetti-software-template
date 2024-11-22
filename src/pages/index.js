656db8eb6e230fcf582dee58f030fb77d683f6ce
confettisoftware <confettisoftware@gmail.com>
2024-11-21 21:19:41 -0800

    Create index.js
    this was missing and causing issues with deployment

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+// For JavaScript: index.js
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Confetti Software!</h1>
+      <p>Your next big app starts here.</p>
+    </div>
+  );
+};
+
+export default Home;
