5015cd1f98d1410f7d07dc099c2a353ab400f9fe
confettisoftware <confettisoftware@gmail.com>
2024-11-24 11:52:07 -0800

    Create support.js
    mathflash5

---
+++ src/pages/support.js
@@ -0,0 +1,14 @@
+const Support = () => {
+    return (
+        <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+            <h1>Support</h1>
+            <p>If you need help with Math Flash or have questions, please contact us:</p>
+            <ul>
+                <li>Email: <a href="mailto:confettisoftware@google.com">confettisoftware@google.com</a></li>
+            </ul>
+            <p>We’re here to help!</p>
+        </div>
+    );
+};
+
+export default Support;
