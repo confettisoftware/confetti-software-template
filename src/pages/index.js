cb29fcaf4fdb33b6b1c9feb15d1c257c8ac78d44
confettisoftware <confettisoftware@gmail.com>
2024-11-24 05:03:59 -0800

    Update index.js
    mathflash2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,23 @@
+import Link from 'next/link';
+
 const Home = () => {
   return (
-    <div>
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
       <h1>Welcome to Confetti Software 🚀</h1>
       <p>Empowering creators to build the next big thing!</p>
+
+      {/* Add a section for your apps */}
+      <section style={{ marginTop: "20px" }}>
+        <h2>Our Apps</h2>
+        <ul>
+          <li>
+            {/* Link to Math Flash */}
+            <Link href="/mathflash">
+              Math Flash
+            </Link> - Learn math with interactive flashcards (Available Soon).
+          </li>
+        </ul>
+      </section>
     </div>
   );
 };
