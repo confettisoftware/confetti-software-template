dd3af53ac85edcc9c642d434045caa2345a5887f
confettisoftware <confettisoftware@gmail.com>
2024-11-24 21:14:49 -0800

    Update navbar.js
    mathflash15

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,28 +1,40 @@
-const Navbar = () => {
-    return (
-        <nav className="bg-gray-800 text-white">
-            <div className="container mx-auto flex justify-between items-center p-4">
-                {/* Logo */}
-                <a href="/" className="text-xl font-bold">Confetti Software</a>
+import Link from 'next/link';
 
-                {/* Links */}
-                <div className="flex items-center space-x-4">
-                    {/* Dropdown for Apps */}
-                    <div className="relative group">
-                        <button className="hover:underline">Apps</button>
-                        <div className="absolute hidden group-hover:block bg-gray-800 text-white mt-2 rounded shadow-lg">
-                            <a href="/mathflash" className="block px-4 py-2 hover:bg-gray-700">Math Flash</a>
-                            {/* Add more app links as needed */}
-                            <a href="/future-app" className="block px-4 py-2 hover:bg-gray-700">Future App</a>
-                        </div>
-                    </div>
-                    {/* Other Links */}
-                    <a href="/support" className="hover:underline">Support</a>
-                    <a href="/privacy-policy" className="hover:underline">Privacy Policy</a>
-                </div>
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 text-white">
+      <div className="container mx-auto flex justify-between items-center p-4">
+        {/* Logo */}
+        <Link href="/">
+          <a className="text-xl font-bold">Confetti Software</a>
+        </Link>
+
+        {/* Links */}
+        <div className="flex items-center space-x-4">
+          {/* Dropdown for Apps */}
+          <div className="relative group">
+            <button className="hover:underline">Apps</button>
+            <div className="absolute hidden group-hover:block bg-gray-800 text-white mt-2 rounded shadow-lg">
+              <Link href="/mathflash">
+                <a className="block px-4 py-2 hover:bg-gray-700">Math Flash</a>
+              </Link>
+              {/* Add more app links here */}
+              <Link href="/future-app">
+                <a className="block px-4 py-2 hover:bg-gray-700">Future App</a>
+              </Link>
             </div>
-        </nav>
-    );
+          </div>
+          {/* Other Links */}
+          <Link href="/support">
+            <a className="hover:underline">Support</a>
+          </Link>
+          <Link href="/privacy-policy">
+            <a className="hover:underline">Privacy Policy</a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
