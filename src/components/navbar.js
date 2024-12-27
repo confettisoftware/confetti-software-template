98a92e5f9dc28e2549b92abf97d2098e7641b778
confettisoftware <confettisoftware@gmail.com>
2024-12-27 02:23:32 -0800

    Update navbar.js
    retry

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,5 @@
+// src/components/Navbar.js
+
 import Link from 'next/link';
 
 const Navbar = () => {
@@ -5,31 +7,33 @@ const Navbar = () => {
     <nav className="bg-gray-800 text-white">
       <div className="container mx-auto flex justify-between items-center p-4">
         {/* Logo */}
-        <Link href="/">
-          <a className="text-xl font-bold">Confetti Software</a>
+        <Link href="/" className="text-xl font-bold hover:underline">
+          Confetti Software
         </Link>
 
         {/* Links */}
         <div className="flex items-center space-x-4">
           {/* Dropdown for Apps */}
           <div className="relative group">
-            <button className="hover:underline">Apps</button>
-            <div className="absolute hidden group-hover:block bg-gray-800 text-white mt-2 rounded shadow-lg">
-              <Link href="/mathflash">
-                <a className="block px-4 py-2 hover:bg-gray-700">Math Flash</a>
+            <button className="hover:underline focus:outline-none">
+              Apps
+            </button>
+            <div className="absolute left-0 mt-2 w-40 bg-gray-800 text-white rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              <Link href="/mathflash" className="block px-4 py-2 hover:bg-gray-700">
+                Math Flash
               </Link>
               {/* Add more app links here */}
-              <Link href="/future-app">
-                <a className="block px-4 py-2 hover:bg-gray-700">Future App</a>
+              <Link href="/future-app" className="block px-4 py-2 hover:bg-gray-700">
+                Future App
               </Link>
             </div>
           </div>
           {/* Other Links */}
-          <Link href="/support">
-            <a className="hover:underline">Support</a>
+          <Link href="/support" className="hover:underline">
+            Support
           </Link>
-          <Link href="/privacy-policy">
-            <a className="hover:underline">Privacy Policy</a>
+          <Link href="/privacy-policy" className="hover:underline">
+            Privacy Policy
           </Link>
         </div>
       </div>
