77c43a0674d49855469cdabb77ef2d13dc1ad56d
confettisoftware <confettisoftware@gmail.com>
2025-03-25 17:54:02 -0700

    Update Layout.js
    update links

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,8 +12,15 @@ const Layout = ({ children }) => {
       <main className="flex-grow container mx-auto p-6">{children}</main>
 
       {/* Footer */}
-      <footer className="bg-gray-800 text-white text-center py-4">
-        &copy; {new Date().getFullYear()} Confetti Software. All rights reserved.
+      <footer className="bg-gray-800 text-white text-center py-4 space-y-2">
+        <div>
+          <a href="/about" className="text-blue-400 hover:underline mx-2">About</a> |
+          <a href="/privacy-policy" className="text-blue-400 hover:underline mx-2">Privacy Policy</a> |
+          <a href="/support" className="text-blue-400 hover:underline mx-2">Support</a>
+        </div>
+        <div>
+          &copy; {new Date().getFullYear()} Confetti Software. All rights reserved.
+        </div>
       </footer>
     </div>
   );
