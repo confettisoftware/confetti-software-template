b6d9504269ec0f20222af8363c1f880cb342c736
confettisoftware <confettisoftware@gmail.com>
2025-06-19 01:55:05 -0700

    Update Layout.js
    footer attempt 2

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,16 +5,16 @@ import Link from 'next/link';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen">
       {/* Navigation Bar */}
       <Navbar />
 
-      {/* Main Content */}
-      <main className="flex-grow container mx-auto p-6">
+      {/* Main Content: flex-1 to fill remaining space */}
+      <main className="flex-1 container mx-auto p-6">
         {children}
       </main>
 
-      {/* Footer */}
+      {/* Footer anchored to bottom */}
       <footer className="mt-auto flex-shrink-0 bg-gray-800 text-white text-center py-4 space-y-2">
         <div>
           <Link href="/about" className="text-blue-400 hover:underline mx-2">About</Link> |
